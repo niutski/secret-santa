b5cd5bbc68991ded919a3078416d20d28aa77f5f
Janne Niutanen <ext-janne.niutanen@elisa.fi>
2017-03-18 14:34:03 +0200

    Echo received message to sender

--- api/fb/handler.js
+++ api/fb/handler.js
@@ -1,19 +1,24 @@
+require('babel-polyfill')
 import config from '../../config'
-
-// TODO: implement "/authorize" endpoint
+import crypto from 'crypto'
+import request from 'request-promise-lite'
 
 export function router(event, context, callback) {
-  if (assertMessageFromFacebook(event, callback)) {
-    switch (event.method) {
-      case 'GET': get(event, context, callback); break;
-      case 'POST': post(event, context, callback); break;
-      default: callback(null, {statusCode: 405, input: JSON.stringify(event)})
-    }
+  switch (event.path.proxy) {
+    case 'webhook': webhook(event, context, callback); break;
+    default: callback(new Error("Access denied"))
+  }
+}
+
+function webhook(event, context, callback) {
+  switch (event.method) {
+    case 'GET': get(event, context, callback); break;
+    case 'POST': post(event, context, callback); break;
+    default: callback(null, {statusCode: 405, input: JSON.stringify(event)})
   }
 }
 
 function get(event, context, callback) {
-  console.log(JSON.stringify(event))
   if (event.query['hub.mode'] === 'subscribe' &&
       event.query['hub.verify_token'] === config.VALIDATION_TOKEN) {
       callback(null, parseInt(event.query['hub.challenge'], 10))
@@ -23,10 +28,59 @@ function get(event, context, callback) {
 }
 
 function post(event, context, callback) {
-  callback(null, {statusCode: 201, input: JSON.stringify(event)})
+  assertMessageFromFacebook(event, callback)
+  echoReceivedMessage(event.body)
+  callback(null, {status: "200"})
 }
 
-function assertMessageFromFacebook(event, callback) {
-  // TODO
-  return true
+function assertMessageFromFacebook(event, callback, ) {
+  var signature = event.headers["X-Hub-Signature"];
+
+  if (!signature) {
+    throw new Error("Couldn't validate the request signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', config.APP_SECRET)
+                        .update(JSON.stringify(event.body), 'UTF-8')
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
+function echoReceivedMessage(messageEvent) {
+  var messageData = {
+    recipient: {
+      id: messageEvent.entry[0].messaging[0].sender.id
+    },
+    message: {
+      text: messageEvent.entry[0].messaging[0].message.text,
+      metadata: "SOMETHING_HERE"
+    }
+  }
+  callSendAPI(messageData)
+}
+
+
+function callSendAPI(json) {
+  const url = "https://graph.facebook.com/v2.6/me/messages"
+  const qs = { access_token: config.PAGE_ACCESS_TOKEN }
+  request.post(url,
+    {
+      body: json,
+      json: true,
+      qs
+    })
+  .then(resp => {
+    console.log("Successfully sent message")
+  })
+  .catch(err => {
+    console.error("Failed to send message:")
+    console.log(err)
+  })
 }
